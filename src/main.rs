24ec703fecfc6c0d8eff9e16bb4016ba80f96b0e
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-06-05 15:11:13 -0700

    checkpoint: working USART1 sample with latest stm32f3xx_hal

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,10 @@
 #![no_main]
 
 // pick a panicking behavior
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
+use panic_rtt_core::{self, rprintln, rtt_init_print};
 
-#[cfg(not(debug_assertions))]
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-#[cfg(debug_assertions)]
-extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
-
-#[cfg(debug_assertions)]
-use cortex_m_log::printer::semihosting;
-
-#[cfg(debug_assertions)]
-use cortex_m_log::println;
-
-#[cfg(debug_assertions)]
-use cortex_m_semihosting::hprintln;
-
-use cortex_m_log::d_println;
-use cortex_m_rt::{entry, ExceptionFrame};
+use cortex_m_rt as rt;
+use rt::{entry};
 
 use stm32f3xx_hal as p_hal;
 
@@ -33,20 +18,9 @@ use embedded_hal::blocking::delay::{DelayMs, DelayUs};
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::digital::v2::ToggleableOutputPin;
 
-use arrayvec::ArrayString;
-use core::fmt;
 
 use p_hal::time::{Hertz, U32Ext};
 
-use cortex_m_rt as rt;
-
-#[cfg(debug_assertions)]
-// type DebugLog = cortex_m_log::printer::dummy::Dummy;
-type DebugLog = cortex_m_log::printer::semihosting::Semihosting<
-    cortex_m_log::modes::InterruptFree,
-    cortex_m_semihosting::hio::HStdout,
->;
-//type DebugLog = cortex_m_log::printer::itm::Itm<cortex_m_log::modes::InterruptFree>
 
 type ImuI2cPortType = p_hal::i2c::I2c<
     I2C1,
@@ -65,20 +39,6 @@ type Usart1PortType = p_hal::serial::Serial<
 >;
 
 
-
-
-// // cortex-m-rt calls this for serious faults.  can set a breakpoint to debug
-// #[exception]
-// fn HardFault(ef: &ExceptionFrame) -> ! {
-//     panic!("{:#?}", ef);
-// }
-
-/// Used in debug builds to provide a logging outlet
-#[cfg(debug_assertions)]
-fn get_debug_log() -> DebugLog {
-    // cortex_m_log::printer::Dummy::new()
-    semihosting::InterruptFree::<_>::stdout().unwrap()
-}
 
 #[cfg(feature = "stm32f3x")]
 fn setup_peripherals() -> (
@@ -106,7 +66,7 @@ fn setup_peripherals() -> (
 
     let delay_source = p_hal::delay::Delay::new(cp.SYST, clocks);
 
-    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+    // let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
     let mut gpioc = dp.GPIOC.split(&mut rcc.ahb);
 
@@ -133,7 +93,7 @@ fn setup_peripherals() -> (
     let gps1_port = {
         let rx = gpiob.pb7.into_af7(&mut gpiob.moder, &mut gpiob.afrl);
         let tx = gpiob.pb6.into_af7(&mut gpiob.moder, &mut gpiob.afrl);
-        p_hal::serial::Serial::usart1(dp.USART1, (tx,rx), 115200.bps(), clocks, &mut rcc.apb2)
+        p_hal::serial::Serial::usart1(dp.USART1, (tx,rx), 9600.bps(), clocks, &mut rcc.apb2)
     };
 
     (i2c_port, user_led1, gps1_port, delay_source)
@@ -145,112 +105,40 @@ fn setup_peripherals() -> (
 #[entry]
 fn main() -> ! {
 
-    let (_i2c_port, mut user_led1, gps1_port, mut delay_source) = setup_peripherals();
-    #[cfg(debug_assertions)]
-    let mut log = get_debug_log();
-    // let i2c_bus = shared_bus::CortexMBusManager::new(i2c_port);
+    rtt_init_print!(NoBlockTrim);
+    rprintln!("-- > MAIN --");
 
+    let (_i2c_port, mut user_led1, gps1_port, mut delay_source) = setup_peripherals();
 
     let _ = user_led1.set_low();
-    d_println!(log, "ready!");
+    rprintln!("ready!");
     delay_source.delay_ms(1u8);
 
-    let (_gps_tx, gps_rx)  = gps1_port.split();
+    let ( _gps_tx,  gps_rx)  = gps1_port.split();
     let mut ublox = ublox_core::new_serial_driver(gps_rx);
     ublox.setup(&mut delay_source).unwrap();
 
-    d_println!(log, "loopstart!");
+    rprintln!("loopstart!");
     loop {
         // check GNSS
         if let Ok(msg_count) = ublox.handle_one_message() {
             //console_print(&mut po_tx, format_args!(">>> msg_count: {} \r\n", msg_count));
             if msg_count > 0 {
                 if let Some(nav_pvt) = ublox.take_last_nav_pvt() {
-                    d_println!(log, ">>> nav_pvt lat, lon: {}, {} \r\n",
+                    rprintln!(">>> nav_pvt lat, lon: {}, {} \r\n",
                             nav_pvt.lat,
                             nav_pvt.lon);
                 }
                 if let Some(nav_dop) = ublox.take_last_nav_dop() {
-                    d_println!(log, ">>> nav_dop {} \r\n", nav_dop.itow);
+                    rprintln!(">>> nav_dop {} \r\n", nav_dop.itow);
                 }
                 if let Some(mon_hw) = ublox.take_last_mon_hw() {
-                    d_println!(log,">>> mon_hw jam: {} \r\n", mon_hw.jam_ind);
+                    rprintln!(">>> mon_hw jam: {} \r\n", mon_hw.jam_ind);
                 }
             }
         }
 
         let _ = user_led1.toggle();
-        delay_source.delay_ms(250u8);
-        delay_source.delay_ms(250u8);
     }
 }
 
-/// Implements exponential weighted moving average of sensor readings,
-/// including exponentially fading minimum and maximum
-pub struct SensorValueTracker {
-    /// recent minimum value (not global minimum)
-    local_min: f64,
-    /// recent maximum value (not global maximum)
-    local_max: f64,
-    /// exponentially weighted moving average
-    average: f64,
-    /// weighting factor-- bigger alpha causes faster fade of old values
-    alpha: f64,
-}
-
-impl SensorValueTracker {
-    pub fn new(alpha: f64) -> Self {
-        Self {
-            local_min: core::f64::NAN,
-            local_max: core::f64::NAN,
-            average: core::f64::NAN,
-            alpha: alpha,
-        }
-    }
-
-    pub fn average(&self) -> f64 {
-        self.average
-    }
-
-    pub fn range(&self) -> f64 {
-        let mut range = self.local_max - self.local_min;
-        if range == 0.0 {
-            range = 1.0;
-        }
-
-        if range < 0.0 {
-            -range
-        } else {
-            range
-        }
-    }
-
-    pub fn update(&mut self, new_value: f64) -> f64 {
-        //seed the EMWA with the initial value
-        if self.local_min.is_nan() {
-            self.local_min = new_value;
-        }
-        if self.local_max.is_nan() {
-            self.local_max = new_value;
-        }
-        if self.average.is_nan() {
-            self.average = new_value;
-        }
-
-        self.average = (self.alpha * new_value) + (1.0 - self.alpha) * self.average;
-
-        // extrema fade toward average
-        if new_value > self.local_max {
-            self.local_max = new_value;
-        } else if new_value > self.average {
-            self.local_max = (self.alpha * new_value) + (1.0 - self.alpha) * self.local_max;
-        }
-        if new_value < self.local_min {
-            self.local_min = new_value;
-        } else if new_value < self.average {
-            self.local_min = (self.alpha * new_value) + (1.0 - self.alpha) * self.local_min;
-        }
-
-        self.average
-    }
-}
